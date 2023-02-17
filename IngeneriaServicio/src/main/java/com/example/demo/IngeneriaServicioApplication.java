3a05647b31d5f571f93c30fcee7b237a4d0b80b5
Usuario <Usuario@DESKTOP-NUDF4OE>
2023-02-17 11:39:21 +0100

    hola mundo

---
+++ IngeneriaServicio/src/main/java/com/example/demo/IngeneriaServicioApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngeneriaServicioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IngeneriaServicioApplication.class, args);
+	}
+// alumno de ingeneria
+}
